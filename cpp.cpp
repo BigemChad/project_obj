0215a9a84c42af8b506ffce3b52943edbdbb4e2f
Aleksander Okupnik <kigemzyt@gmail.com>
2024-03-05 13:07:26 +0100

    v1.1

--- cpp.cpp
+++ cpp.cpp
@@ -1,9 +1,94 @@
-#include <iostream>
-#include <string>
-#include <sstream>
-#include <vector>
+//Windows
+// #include <iostream>
+// #include <string>
+// #include <sstream>
+// #include <vector>
+
+//Linux
+#include<bits/stdc++.h>
 
 using namespace std;
+
+struct Node {
+    string name;
+    Node* next;
+};
+
+
+//class leaf
+class leaf{
+    public:
+        char name;
+        void addMember(Node &head, string toAdd){
+            Node* temp = &head;
+            while(temp->next!= NULL){
+                temp = temp->next;
+            }
+            temp->next = new Node;
+            temp = temp->next;
+            temp->name = toAdd;
+            temp->next = NULL;
+        }
+
+        void printList(Node &head) {
+            Node* temp = &head;
+            int i = 1;
+            while(temp->next!= NULL){
+                cout << i << temp->name << '\n';
+                temp = temp->next;
+                i++;
+            }
+            i++;
+            cout << temp->name << endl;
+        }
+    private:
+    int value;
+    Node* head = new Node;
+
+};
+
+//class branch
+class branch{
+    public:
+        char name;
+        int n;
+        branch(int hM, char letter){
+            n = hM;
+            name = letter;
+        }
+        void displayMembers(vector<leaf>& leaves) {
+           for(auto el : leaves)
+                cout << el.name << " ";
+            cout << '\n';
+        }
+        void addMembers(vector<leaf>& leaves, int &n) {
+            leaf temp;
+            for(int i = 0; i < n; i++)
+                leaves.push_back(temp);
+        }
+
+    private:
+        int value;
+        vector<leaf> leaves;
+
+};
+
+class root{
+    public:
+        char name = 'A';
+        vector<branch> branches;
+        void addBranches(vector<branch>& branches) {
+            branch temp1(1, 'B');
+            branch temp2(2, 'C');
+            branch temp3(3, 'D');
+            branches.push_back(temp1);
+            branches.push_back(temp2);
+            branches.push_back(temp3);
+        }
+
+    private:
+        int value = 1;
+};
 
 // Prototypy (wrzucić do drugiego pliku)
 void commandCD(const string& object);
